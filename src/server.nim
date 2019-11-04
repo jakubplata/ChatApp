4a720d36c17b35612511cf4e71165f1b8d412ec4
Jakub Plata <jakubplata@users.noreply.github.com>
2019-11-04 14:46:46 +0000

    Add server module

---
+++ src/server.nim
@@ -0,0 +1,15 @@
+import asyncdispatch, asyncnet
+
+type
+  Client = ref object
+    socket: AsyncSocket
+    netAddr: string
+    id: int
+    connected: bool
+
+  Server = ref object
+    socket: AsyncSocket
+    clients: seq[Client]
+
+proc newServer(): Server = Server(socket: newAsyncSocket(), clients: @[])
+var server = newServer()
